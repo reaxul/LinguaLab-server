9391d0e0e5e06bfb4d6c0f5e4cac7fd1f43d9766
DevReaxul <hreazul11@gmail.com>
2023-06-29 02:50:07 +0600

    user data updated

--- index.js
+++ index.js
@@ -65,6 +65,11 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({message: 'User already exists'})
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
